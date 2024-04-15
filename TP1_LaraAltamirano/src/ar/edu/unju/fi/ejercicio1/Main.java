6901d3b5419159ded46200d4815fbd11202bedac
wiber <wiber@DESKTOP-29TD6KD>
2024-04-15 19:50:26 -0300

    Se agrega el paquete y clase para el ejercicio 1

---
+++ TP1_LaraAltamirano/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int N1 = 8 , N2 = 5, N3 = 1, N4 = 20, N5 = 9;
+		double promedio = (N1+N2+N3+N4+N5)/5.0;
+		System.out.println("El promedio es: "+ promedio);
+		
+	}
+
+}
